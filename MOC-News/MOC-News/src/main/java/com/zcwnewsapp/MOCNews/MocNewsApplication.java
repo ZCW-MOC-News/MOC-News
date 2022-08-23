61fc5bd41d83aad45d5d4e965240fe77577d9484
Meilin (Linda) Li <106841467+limeilinda@users.noreply.github.com>
2022-08-23 10:23:22 -0400

    Add files via upload

---
+++ MOC-News/MOC-News/src/main/java/com/zcwnewsapp/MOCNews/MocNewsApplication.java
@@ -0,0 +1,13 @@
+package com.zcwnewsapp.MOCNews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MocNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MocNewsApplication.class, args);
+	}
+
+}
---
+++ MOC-News/MOC-News/src/test/java/com/zcwnewsapp/MOCNews/MocNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zcwnewsapp.MOCNews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MocNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
