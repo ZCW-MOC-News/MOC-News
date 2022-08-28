caab8c69fefda9eeb97d3e8ca73a7695140e9b19
Meilin Li (Linda) <limeilinda@gmail.com>
2022-08-27 20:55:17 -0400

    Commented code that is not working for now

--- MOC-News/MOC-News/src/main/java/com/zcwnewsapp/MOCNews/comments/Comment.java
+++ MOC-News/MOC-News/src/main/java/com/zcwnewsapp/MOCNews/comments/Comment.java
@@ -1,25 +1,25 @@
-package com.zcwnewsapp.MOCNews.comments;
-
-import lombok.Getter;
-import lombok.Setter;
-
-import javax.persistence.*;
-
-@Setter @Getter
-@Entity
-public class Comment {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name="comment_id")
-    private Long id;
-    private String comment;
-
-
-    public Comment() {
-
-    }
-
-
-
-}
+//package com.zcwnewsapp.MOCNews.comments;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//import javax.persistence.*;
+//
+//@Setter @Getter
+//@Entity
+//public class Comment {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @Column(name="comment_id")
+//    private Long id;
+//    private String comment;
+//
+//
+//    public Comment() {
+//
+//    }
+//
+//
+//
+//}
--- MOC-News/MOC-News/src/main/java/com/zcwnewsapp/MOCNews/comments/CommentController.java
+++ MOC-News/MOC-News/src/main/java/com/zcwnewsapp/MOCNews/comments/CommentController.java
@@ -1,15 +1,24 @@
-package com.zcwnewsapp.MOCNews.comments;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping(path="/comment")
-public class CommentController {
-
-    @Autowired
-    private CommentRepository commentRepository;
-
-}
+//package com.zcwnewsapp.MOCNews.comments;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//@Controller
+//@RequestMapping(path="/comment")
+//public class CommentController {
+//
+//    @Autowired
+//    private CommentRepository commentRepository;
+//
+//    @PostMapping(path="/post")
+//    public @ResponseBody String addComment (@RequestParam String comment) {
+//        Comment c = new Comment();
+//        c.setComment(comment);
+//        return "Saved";
+//    }
+//
+//}
