66349744e29cec6481a7673947b8377d461addc4
Chris Kent <Chris_Kent@ymail.com>
2022-08-29 20:43:10 -0400

    Added Bookmark classes

--- MOC-News/MOC-News/src/main/java/com/zcwnewsapp/MOCNews/bookmarks/Bookmark.java
+++ MOC-News/MOC-News/src/main/java/com/zcwnewsapp/MOCNews/bookmarks/Bookmark.java
@@ -1,4 +1,34 @@
 package com.zcwnewsapp.MOCNews.bookmarks;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.zcwnewsapp.MOCNews.article.Article;
+import com.zcwnewsapp.MOCNews.user.Account;
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(uniqueConstraints= @UniqueConstraint(columnNames = {"article_id", "account_id"}) )
 public class Bookmark {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "bookmarks_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_id")
+    @JsonIgnore
+    private Article article;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id")
+    @JsonIgnore
+    private Account account;
+
+    public Bookmark() {
+    }
+
 }
---
+++ MOC-News/MOC-News/src/main/java/com/zcwnewsapp/MOCNews/bookmarks/BookmarkController.java
@@ -0,0 +1,41 @@
+package com.zcwnewsapp.MOCNews.bookmarks;
+
+import com.zcwnewsapp.MOCNews.article.Article;
+import com.zcwnewsapp.MOCNews.user.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@Controller
+@RequestMapping(path="/bookmark")
+public class BookmarkController {
+
+    @Autowired
+    private BookmarkRepository bookmarkRepository;
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Transactional
+    @PostMapping(path="/add")
+    public @ResponseBody String addBookmark(@RequestParam Long account_id, @RequestParam Long article_id) {
+        Bookmark bookmark = new Bookmark();
+        Account acc = entityManager.getReference(Account.class, account_id);
+        Article art = entityManager.getReference(Article.class, article_id);
+        bookmark.setAccount(acc);
+        bookmark.setArticle(art);
+        entityManager.persist(bookmark);
+        bookmarkRepository.save(bookmark);
+        return "Saved";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<Bookmark> getAllBookmarks() {
+        return bookmarkRepository.findAll();
+    }
+
+}
